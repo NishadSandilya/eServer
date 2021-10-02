4ba5e9f0806c49acebdca6f3fb7594334f08f4db
Nishad Sandilya <67221201+NishadSandilya@users.noreply.github.com>
2021-10-02 18:55:20 +0530

    fixing razorpay

--- routers/razorpayRouter.js
+++ routers/razorpayRouter.js
@@ -1,5 +1,6 @@
 const { auth } = require('../controllers/authController')
 const { newPayment, webhookCapture } = require('../controllers/razorpayController')
+const { asyncHandler } = require('../utils/asyncHandler')
 
 const router = require('express').Router()
 
@@ -10,5 +11,16 @@ router
 router
     .route('/verifyPayment')
     .post(webhookCapture)
+
+router
+    .route('/handle-redirect')
+    .post(asyncHandler(async(req, res, next) => {
+        //redirect to erida
+        res
+            .header("Access-Control-Allow-Origin", "https://erida.in")
+            .header('Access-Control-Allow-Credentials', true)
+            .header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+            .redirect('https://erida.in/#/orders')
+    }))
 
 module.exports = router
