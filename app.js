da02708e020f15dea1d0884b23528fd45908f063
Nishad Sandilya <theesaan@gmail.com>
2022-03-29 12:45:59 +0530

    Updates

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ const whitelist = ['https://nishadsandilya.github.io', 'http://localhost:3000', 
 
 const corsOptions = {
     origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1 || origin) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
