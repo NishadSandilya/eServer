c437c4fee3d698949093d7cf2a891bc10922f331
Nishad Sandilya <theesaan@gmail.com>
2022-03-29 12:16:30 +0530

    Fixing CORS

--- utils/response.js
+++ utils/response.js
@@ -1,7 +1,7 @@
 //Generic response function for controllers
 exports.respond = (statusCode, message, payload, res) => {
     res
-        // .header("Access-Control-Allow-Origin", "https://erida.in")
+        .header("Access-Control-Allow-Origin", "https://erida.in")
         .header('Access-Control-Allow-Credentials', true)
         .header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
         .status(statusCode)
